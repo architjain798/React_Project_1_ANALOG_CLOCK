680dffcedeafeae1331519d73175c618f401125e
Archit Jain <77613361+architjain798@users.noreply.github.com>
2021-02-08 23:06:15 +0530

    Version_1 Clock working number is showing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import "./styles.css";
-
-
-
 export default function App() {
   return (
     <>
@@ -9,22 +6,20 @@ export default function App() {
         <div id="hour"></div>
         <div id="minute"></div>
         <div id="second"></div>
-        {
-          setInterval(() => {
-    let d = new Date();
-    let htime = d.getHours();
-    let mtime = d.getMinutes();
-    let stime = d.getSeconds();
-    let hrotation = 30*htime + mtime/2;
-    let mrotation = 6*mtime;
-    let srotation = 6*stime;
+        {setInterval(() => {
+          let d = new Date();
+          let htime = d.getHours();
+          let mtime = d.getMinutes();
+          let stime = d.getSeconds();
+          let hrotation = 30 * htime + mtime / 2;
+          let mrotation = 6 * mtime;
+          let srotation = 6 * stime;
 
-    hour.style.transform = `rotate(${hrotation}deg)`;
-    minute.style.transform = `rotate(${mrotation}deg)`;
-    second.style.transform = `rotate(${srotation}deg)`;
-}, 1000);
-}
-    </div>
+          hour.style.transform = `rotate(${hrotation}deg)`;
+          minute.style.transform = `rotate(${mrotation}deg)`;
+          second.style.transform = `rotate(${srotation}deg)`;
+        }, 1000)}
+      </div>
     </>
   );
 }
